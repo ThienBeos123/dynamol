3cc70ae691f929f9420503a27ffff3ccbce7e4e0
admin <thien190911@gmail.com>
2026-06-07 23:23:24 +0700

    feat(calc): finish setup for unit tests of calculational algorithms

---
+++ test/calc/perf_mul_test.c
@@ -0,0 +1,16 @@
+// Core Types & Utilities
+#include <include.h>
+#include <dnml_status.h>
+#include <dnml_sys/sys.h>
+#include <libdnml_types.h>
+// Memory Management and Algorithmic core
+#include <_libdnml_config/numeric_config.h>
+#include <_libdnml_mem/arena.h>
+#include <_libdnml_mem/_ctx.h>
+#include "../../calc_algo/perf_based/mul.h"
+
+
+int main(void) {
+    // code here ...
+    return 0;
+}
